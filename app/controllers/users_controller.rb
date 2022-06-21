c1f14959bc3109c05a93a8c0cd444ad164d693af
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-06-21 14:27:15 +0000

    [Add]4-15Meshiterro_マイページ作成完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    # 特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し、@post_imagesに渡す
+    # 全体の投稿ではなく、個人が投稿したもの全てを表示できる
+    @post_images = @user.post_images
   end
 
   def edit
