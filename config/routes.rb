35bf6be0e2e03cfdbd546c3f0448b56c16dfe5b7
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-06-23 13:41:24 +0000

    [Add]4-18Meshiterro_コメント投稿と削除

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@ Rails.application.routes.draw do
   # get 'users/edit'
   root to: "homes#top"
   devise_for :users
+  get 'homes/about' => 'homes#about', as:'about'
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
   # を書き直し
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  # コメントは投稿に対して、むすびつくので親子関係に[ネストするの状態]
+  # そうしてparams[:post_image_id]でPostImageのidが取得できるようになる
+   resources :post_comments, only: [:create, :destroy]
+  end
+  
   resources :users, only: [:show, :edit, :update]
-  get 'homes/about' => 'homes#about', as:'about'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
